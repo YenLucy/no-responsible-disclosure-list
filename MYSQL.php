33d6197c199092d2e681efeec9de7920703c22b9
YenLucy <contact@better-save-then-sorry.de>
2021-10-15 11:54:41 +0200

    fixed a small problem

--- MYSQL.php
+++ MYSQL.php
@@ -4,5 +4,5 @@ $MYSQL_USER = "user";					// Mysql-Username
 $MYSQL_PASS = "pass";					// Mysql-Password
 $MYSQL_HOSTIP = "docker-mysql";			// Mysql-IP-Adresse
 $MYSQL_PORT = "3306";					// Mysql-Portnr
-$MYSQL_DATABASE = "Cardinal_System"; 	// Mysql-Datenbank
+$MYSQL_DATABASE = "its-your-fault"; 	// Mysql-Datenbank
 ?>
